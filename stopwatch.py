0a957e7a511586970cc601f3c80593c21d92c2cd
poornapragna877 <54894632+poornapragna877@users.noreply.github.com>
2019-10-05 23:19:15 +0530

    Update stopwatch.py
    Small Typo: "changed lapTine to lapTime" .

--- stopwatch.py
+++ stopwatch.py
@@ -10,10 +10,10 @@ lapNum = 1
 try :
 	while True :
 		input()
-		lapTine=round(time.time() - lastTime, 2)
+		lapTime=round(time.time() - lastTime, 2)
 		totalTime=round(time.time() - startTime ,2)
 		print('Lap #%s: %s' %(lapNum, totalTime))
-		print('Time between Lap #%s and Lap #%s: %s' %(lapNum-1, lapNum, lapTine), end='')
+		print('Time between Lap #%s and Lap #%s: %s' %(lapNum-1, lapNum, lapTime), end='')
 		print('    ')
 		lapNum+=1;
 		lastTime=time.time()
